811b6345c19ebb4115154d5bad00e157d8f6defa
comahead <comahead@gmail.com>
2018-06-26 18:13:55 +0900

    ..

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use('/api/v1', v1());
 
 app.use('/', function(req, res){
     res.statusCode = 200;//send the appropriate status code
-    res.json({status:"success", message:"Parcel Pending API", data:{}})
+    res.json({status:"success", message:"welcome to words API", data:{}})
 });
 
 // catch 404 and forward to error handler
